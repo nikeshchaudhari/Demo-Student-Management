aa13fd378aa36e8e17aadb078eaa8633be40ea7c
Nikesh <info.nikesh12@gmail.com>
2025-06-18 00:22:27 +0545

    demo std management

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import StudentRegister from './component/StudentRegister';
+import Login from './component/Login'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 function App() {
+  const myroute = createBrowserRouter([
+    {path:"/",element:<Login/>},
+    {path:"/studentregister",element:<StudentRegister/>}
+  ]
+
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+<RouterProvider router={myroute}/>
+
+
+
+    
     </div>
   );
 }
