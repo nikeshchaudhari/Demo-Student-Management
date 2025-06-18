cb53a478ab8522cff6fa97480eeecc23d31ae912
Nikesh <info.nikesh12@gmail.com>
2025-06-18 13:54:22 +0545

    backend

---
+++ backend/models/StudentDetails.js
@@ -0,0 +1,11 @@
+const { text } = require("body-parser")
+const mongoose = require("mongoose")
+const studentSchema = new mongoose.Schema({
+    s_id:{type:mongoose.Schema.Types.ObjectId},
+    fullName:{type:String,requied:true},
+    email:{type:String,required:true},
+    password:{type:String,requird:true},
+    address :{type:String,required:true},
+    phone:{type:String,required:true}
+})
+module.exports = mongoose.model("student",studentSchema);
