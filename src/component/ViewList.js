ee831390126c26693bdc4b4b0f3345eefdfd473b
Nikesh <info.nikesh12@gmail.com>
2025-06-20 00:48:54 +0545

    management

--- src/component/ViewList.js
+++ src/component/ViewList.js
@@ -1,69 +1,73 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// const StudentDetails = [
-//   { id: 1, fullname: "Hari gautam", email: "hari@gmail.com", phone: "9800000",address:"ktm"},
-//   {
-//     id: 2,
-//     fullname: "sagar gautam",
-//     email: "hari@gmail.com",
-//     phone: "9800000",
-//     address:"ktm"
-//   },
-//   { id: 3, fullname: "mani gautam", email: "hari@gmail.com", phone: "9800000",address:"Birjung" },
-// ];
+
 const ViewList = () => {
-  const [students, setStudent] = useState([]);
+  const [student, setStudent] = useState([]);
+  const token = sessionStorage.getItem("token");
+
   useEffect(() => {
     const viewData = async () => {
       try {
         const view = await axios.get(
-          "http://localhost:8080/student/view"
+          "http://localhost:8080/student/viewstudent",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
-        console.log(view.data);
-        setStudent(view.data);
+        // console.log(view.data);
+        setStudent(view.data.data);
       } catch (err) {
         console.error("Error Data....");
       }
     };
     viewData();
   }, []);
+  const deleteData = (e) => {
+    e.preventDefault();
+    console.log("Delete Data...");
+  };
 
   return (
-    <div className="bg-white max-w-4xl mx-auto p-6 shadow-lg rounded-lg mt-10">
+    <div className="bg-white max-w-4xl sm:w-[full] mx-auto p-6 shadow-lg rounded-lg mt-10">
       <h1 className="text-3xl font-semibold mb-6 text-center">Student List</h1>
       {/* Searchbox */}
       <input
         type="text"
         placeholder="Search student by name..."
-        className="w-full border border-gray-500 p-2 mb-4  rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full sm:w-[full] border border-gray-500 p-2 mb-4  rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       {/* Table  */}
       <table className="w-full border border-collapse  border-x-gray-500">
         <thead>
           <tr className="bg-gray-200">
-            <th className="p-3 border border-gray-300">#</th>
+            <th className="p-3 border border-gray-300">S.N.</th>
             <th className="p-3 border border-gray-300">Fullname</th>
-            <th className="p-3 border border-gray-300">Email</th>
             <th className="p-3 border border-gray-300">Address</th>
             <th className="p-3 border border-gray-300">Phone</th>
+            <th className="p-3 border border-gray-300">Course</th>
             <th className="p-3 border border-gray-300">Action</th>
           </tr>
         </thead>
         <tbody className="text-center">
-          {students.map((s, i) => (
+          {student.map((s, i) => (
             <tr key={i}>
-              <td>1</td>
+              <td>{s.uId}</td>
               <td>{s.fullName}</td>
-              <td>{s.email}</td>
               <td>{s.address} </td>
               <td>{s.phone} </td>
-            
+              <td>{s.course} </td>
+
               <td className="text-center ">
-                <button className="bg-green-600 mx-3 p-2 mb-2 rounded-sm text-white mt-2">
+                <button className="bg-green-600 hover:bg-green-800 mx-3 p-2 mb-2 rounded-lg text-white mt-2">
                   Edit
                 </button>
-                <button className="bg-red-600 mx-6 p-2 mb-2 rounded-sm text-white mt-2">
+                <button
+                  onClick={deleteData}
+                  className="bg-red-600 hover:bg-red-800 mx-6 p-2 mb-2 rounded-lg text-white mt-2"
+                >
                   Delete
                 </button>
               </td>
