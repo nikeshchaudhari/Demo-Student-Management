c5492c84e0833cec659d62e1fe119a5ec5d695cf
Nikesh <info.nikesh12@gmail.com>
2025-06-20 00:51:05 +0545

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,29 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const cors = require("cors")
+const User = require("./user")
+const mongoose = require("mongoose");
+const studentRoutes = require("./student")
+
+require('dotenv').config()
 
 const dbConnect =async () => {
   try {
-    await mongoose.connect()
-  } catch (err) {}
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Database connected....");
+    
+  } catch (err) {
+    console.log("Something is wrong....");
+    console.log(err);
+    
+    
+  }
 };
+dbConnect();
 
 app.use(bodyParser.json());
-
-module.exports = app;
+app.use(cors())
+app.use("/student", studentRoutes);
+app.use("/user",User)
+module.exports = app; 
