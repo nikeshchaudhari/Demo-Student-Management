b83482cbf26ac97c5cf84185f6cc805212330a66
Nikesh <info.nikesh12@gmail.com>
2025-06-20 00:49:56 +0545

    backend

--- backend/models/Signup.js
+++ backend/models/Signup.js
@@ -1,11 +1,12 @@
 const { text } = require("body-parser")
 const mongoose = require("mongoose")
 const studentSchema = new mongoose.Schema({
-    s_id:{type:mongoose.Schema.Types.ObjectId},
+
     fullName:{type:String,requied:true},
     email:{type:String,required:true},
     password:{type:String,requird:true},
     address :{type:String,required:true},
-    phone:{type:String,required:true}
+    phone:{type:String,required:true},
+
 })
-module.exports = mongoose.model("student",studentSchema);
+module.exports = mongoose.model("user",studentSchema);
--- backend/models/StudentDetails.js
+++ backend/models/StudentDetails.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema({
-    fullName :{type:String,require:true},
-    address :{type:String,require:true},
-    phone :{type:String,require:true},
-    course :{type:String,require:true},
-
-})
-module.exports = mongoose.model("student",studentSchema);
+  uId:{ type: mongoose.Schema.Types.ObjectId, require: true, ref: "user" },
+  fullName: { type: String, require: true },
+  address: { type: String, require: true },
+  phone: { type: String, require: true },
+  course: { type: String, require: true },
+});
+module.exports = mongoose.model("student", studentSchema);
